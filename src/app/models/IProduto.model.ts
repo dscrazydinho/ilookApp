ca016dc46b325fac1ecb0772ef62ed23cf6e9753
Alessandro Nunes <alessandro.nunes.cr@gmail.com>
2021-05-26 16:57:58 -0300

    Array de filmes e interface de filmes

---
+++ src/app/models/IProduto.model.ts
@@ -0,0 +1,8 @@
+export interface IProduto {
+  nome: string;
+  data: string;
+  duracao: string;
+  classificacao: number;
+  capa: string;
+  generos: string[];
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
+import { IProduto } from '../models/IProduto.model';
+
+
 
 @Component({
   selector: 'app-tab1',
@@ -8,6 +11,47 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+  titulo = 'Ilook App';
+
+  listaFilmes: IProduto[] = [
+    {
+      nome: 'Mortal Kombat (2021)',
+      data: '15/04/2021',
+      duracao: '1h 50m',
+      classificacao: 76,
+      capa:'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ijvC2w2yANsfgLT3LMu2zFr0fxh.jpg',
+      generos: [
+        'Ação',
+        'Fantasia',
+        'Aventura'
+      ]
+    },
+    {
+      nome: 'Sem Remorso (2021)',
+      data: '30/04/2021',
+      duracao: '1h 50m',
+      classificacao: 73,
+      capa:'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/uHEZ4ZMziIjlAgCTQAEh9ROvtj0.jpg',
+      generos: [
+        'Ação',
+        'Thriller',
+        'Guerra'
+      ]
+    },
+    {
+      nome: 'Mortal Kombat (1994)',
+      data: '15/04/2021',
+      duracao: '1h 50m',
+      classificacao: 76,
+      capa:'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ijvC2w2yANsfgLT3LMu2zFr0fxh.jpg',
+      generos: [
+        'Ação',
+        'Fantasia',
+        'Aventura'
+      ]
+    }
+  ];
 
   constructor(public alertController: AlertController, public toastController: ToastController) { }
 
